7715d319ee3a7a63625fbecce14edfaf21379a20
carlyleec <carlyleec@gmail.com>
2016-06-29 16:33:17 -0400

    the queries are working, but I need to push the database

--- routes/api.js
+++ routes/api.js
@@ -8,16 +8,16 @@ var router = express.Router();
 function getSlopeByPin(pins){
   console.log(pins);
   //Get the SQL statement to determine if the geometry has already been processed
-  var sqlStatement = createSqlStatementToCheckIfGeomIsAlreadyCalcuatedForPins(pins);
+  var sqlStatement = getSqlStatementToCheckIfGeomIsAlreadyCalcuatedForPins(pins);
 
   //returns id of 0 if no matches.
   var slopeId = getCalculatedSlopeId(sqlStatement);
 
-  if(slopeId === 0){
-    sqlStatment = createSqlStatementToCalculateSlopeGeomForPins(pins);
-    //get id of for new slope calculation
-    slopeId = getCalculatedSlopeId(sqlStatment);
-  }
+  // if(slopeId === 0){
+  //   sqlStatment = createSqlStatementToCalculateSlopeGeomForPins(pins);
+  //   //get id of for new slope calculation
+  //   slopeId = getCalculatedSlopeId(sqlStatment);
+  // }
 
   return getSlopeById(slopeId);
 }
@@ -25,8 +25,8 @@ function getSlopeByPin(pins){
 //checks to see if slope already calculated
 //by compareing the input geomery with past geometry
 //returns id of 0 if no matches.
-function createSqlStatementToCheckIfGeomIsAlreadyCalcuatedForPins(pins){
-  var sqlStatementInner = "(SELECT st_union(p.the_geom) as \"GEOM\" " +
+function getSqlStatementToCheckIfGeomIsAlreadyCalcuatedForPins(pins){
+  var sqlStatementInner = "(SELECT st_union(p.shape) as \"GEOM\" " +
                    "FROM \"bc_property\" p " +
                    "WHERE  p.\"pinnum\" in ('" + pins + "'))";
   var sqlStatement = "SELECT slopetool_checkGeomg(" + sqlStatementInner + ")as \"ID\" ";
@@ -37,47 +37,77 @@ function createSqlStatementToCheckIfGeomIsAlreadyCalcuatedForPins(pins){
 function getCalculatedSlopeId(sqlStatement){
   var slopeResultId = 0;
 
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query(sqlStatement, function(err, result) {
-      done();
-      if (err){
-        console.error(err); response.send("Error " + err);
+  var client = new pg.Client(process.env.DATABASE_URL);
+  client.connect(function (err) {
+    if (err) throw err;
+
+    client.query(sqlStatement, function (err, result) {
+      if (err) throw err;
+      console.log("SLOPEID: " + result.rows[0].ID);
+      var slopeId = result.rows[0].ID;
+      if(slopeId === 0){
+        //Do other query
       }else{
-        console.log(results)
+        client.query(getSqlStatementThatSelectSlopeAttributesById(result.rows[0].ID), function (err, result) {
+          if (err) throw err;
+          return result.rows[0];
+
+          // disconnect the client
+          client.end(function (err) {
+            if (err) throw err;
+          });
+        });
       }
+      // disconnect the client
+      client.end(function (err) {
+        if (err) throw err;
+      });
     });
   });
 
   return slopeResultId;
 }
 
-function createSqlStatementToCalculateSlopeGeomForPins(pin){
-  sqlStatment = "SELECT slopetool_getSlopeFromGeom(st_union(p.the_geom)) as \"ID\" " +
+function getSqlStatementToCalculateSlopeGeomForPins(pin){
+  sqlStatment = "SELECT slopetool_getSlopeFromGeom(st_union(p.shape)) as \"ID\" " +
                 "FROM \"bc_property\" p " +
                 "WHERE  p.\"pinnum\" in ('" + pin + "')";
   return sqlStatment;
 }
 
-function getSlopeById(slopeId){
-  var sqlStatement = "SELECT "+slopeId+ " as \"ID\" ,\"JURISDICTION\" as \"Jurisdiction\", " +
-                    "round(\"MAXCONTOUR\",2) as \"Maxium Elevation\",  " +
-                    "round(\"ACRES\",2) as \"Acres\", " +
-                    "round(\"AVGSLOPE\",2) as \"Average Precent Slope\" " +
-                    "FROM \"SlopeResults\" " +
-                    "WHERE \"INSERTID\" = " + slopeId;
+function getSqlStatementThatSelectSlopeAttributesById(slopeId){
+  var sqlStatement = "SELECT "+slopeId+ " as \"id\" ,\"jurisdiction\" as \"jurisdiction\", " +
+        "round(\"maxcontour\",2) as \"maxElevation\",  " +
+        "round(\"acres\",2) as \"acres\", " +
+        "round(\"avgslope\",2) as \"percentSlope\" " +
+        "FROM \"sloperesults\" " +
+        "WHERE \"insertid\" = " + slopeId;
+  return sqlStatement
+}
 
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query(sqlStatement, function(err, result) {
-      done();
-      if (err){
-        console.error(err); response.send("Error " + err);
-      }else{
-        console.log(results)
-      }
+function getSlopeById(slopeId){
+  console.log(slopeId);
+  var sqlStatement = "SELECT "+slopeId+ " as \"ID\" ,\"jurisdiction\" as \"Jurisdiction\", " +
+                    "round(\"maxcontour\",2) as \"Maxium Elevation\",  " +
+                    "round(\"acres\",2) as \"Acres\", " +
+                    "round(\"avgslope\",2) as \"Average Precent Slope\" " +
+                    "FROM \"sloperesults\" " +
+                    "WHERE \"insertid\" = " + slopeId;
+
+  var client = new pg.Client(process.env.DATABASE_URL);
+  client.connect(function (err) {
+    if (err) throw err;
+
+    client.query(sqlStatement, function (err, result) {
+      if (err) throw err;
+      return result.rows[0];
+
+      // disconnect the client
+      client.end(function (err) {
+        if (err) throw err;
+      });
     });
   });
-
-  return slope;
 
 }
 
@@ -88,60 +118,54 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/slope', function(req, res, next) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * from bc_property limit(10)', function(err, result) {
-      done();
-      if (err){
-        res.render('slope', "Well that didn't work!");
-      }else{
-        res.render('slope', {result: JSON.stringify(result)});
-      }
-    });
-  });
-
+  res.render('slope');
 });
 
 router.get('/slope/:pin', function(req, res, next) {
   //getSlopeByPin(req.params.pin);
-  // var client = new pg.Client(process.env.DATABASE_URL);
-  // client.connect(function (err) {
-  //   if (err) throw err;
-  //
-  //   // execute a query on our database
-  //   client.query('SELECT *   FROM public.bc_property limit(10)', function (err, result) {
-  //     if (err) throw err;
-  //     for (var i = 0; i < result.rows.length; i++) {
-  //       console.log(result.rows[i]);
-  //     }
-  //     // just print the result to the console
-  //
-  //
-  //     // disconnect the client
-  //     client.end(function (err) {
-  //       if (err) throw err;
-  //     });
-  //   });
-  // });
-
-
-
-
-  pg.connect(process.env.DATABASE_URL, function(err, client) {
+  var client = new pg.Client(process.env.DATABASE_URL);
+  client.connect(function (err) {
     if (err) throw err;
-    console.log("This is the DATABASE_URL " + JSON.stringify(process.env));
-    console.log('Connected to postgres! Getting schemas...');
 
-    client
-      .query('SELECT * from bc_property limit(10)')
-      .on('row', function(row) {
-        console.log(JSON.stringify(row));
-      });
+    client.query(getSqlStatementToCheckIfGeomIsAlreadyCalcuatedForPins(req.params.pin), function (err, result) {
+      if (err) throw err;
+      var slopeId = result.rows[0].ID;
+      console.log("slopeId #1: " + result.rows[0].ID);
+      if(slopeId == 0){
+        client.query(getSqlStatementToCalculateSlopeGeomForPins(req.params.pin), function (err, result) {
+          if (err) throw err;
+          console.log("slopeId #2: " + JSON.stringify(result.rows[0].ID))
+          var slopeId = result.rows[0].ID;
+          client.query(getSqlStatementThatSelectSlopeAttributesById(slopeId), function (err, result) {
+            if (err) throw err;
+            res.json(result.rows[0]);
+            //Close the connection
+            client.end(function (err) {
+              if (err) throw err;
+            });
+          });
+        });
+
+
+      }else{
+        client.query(getSqlStatementThatSelectSlopeAttributesById(slopeId), function (err, result) {
+          if (err) throw err;
+          res.json(result.rows[0]);
+        });
+        //Close the connection
+        client.end(function (err) {
+          if (err) throw err;
+        });
+      }
+    });
+
+
   });
 
-  res.json({
-    "maxElevation": "1111.00",
-    "percentSlope": "11.11"
-  })
+  // res.json({
+  //   "maxElevation": "1111.00",
+  //   "percentSlope": "11.11"
+  // })
 });
 
 
