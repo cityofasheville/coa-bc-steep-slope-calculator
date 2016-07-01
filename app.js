de0e6d02f91e26e88953ae2737ea44c609c493a7
carlyleec <carlyleec@gmail.com>
2016-07-01 14:53:53 -0400

    added a more user friendly way to search by PIN

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var api = require('./routes/api');
+var findslopeforparcel = require('./routes/findslopeforparcel');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/api', api);
+app.use('/find-slope-for-parcel', findslopeforparcel);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,7 @@
 function addEventHandler(elem, eventType, handler) {
+  if(elem === null){
+    return false;
+  }
   if (elem.addEventListener)
     elem.addEventListener (eventType, handler, false);
   else if (elem.attachEvent)
@@ -8,10 +11,26 @@ function addEventHandler(elem, eventType, handler) {
 addEventHandler(document, 'DOMContentLoaded', function() {
 
     addEventHandler(document.getElementById('testSlopeByPinApi'), 'click', function() {
-      console.log(document.getElementById('testSlopePin').value);
-        window.open("/api/slopebypin/" + document.getElementById('testSlopePin').value, "_blank");
+      window.open("/api/slopebypin/" + document.getElementById('testSlopePin').value, "_blank");
     });
-    // addEventHandler(document.getElementById('testSlopePinForm'), 'submit', function() {
-    //
-    // });
+    addEventHandler(document.getElementById('calcButton'), 'click', function() {
+      document.getElementById('loading').classList.remove("hidden");
+      document.getElementById('loading').classList.add("show");
+      axios.get('/api/slopebypin/' + document.getElementById("pinInput").value)
+        .then(function (response) {
+          document.getElementById('loading').classList.remove("show");
+          document.getElementById('loading').classList.add("hidden");
+          document.getElementById('resultsDiv').classList.remove("hidden");
+          document.getElementById('resultsDiv').classList.add("show");
+          document.getElementById("jurisdiction").innerHTML = response.data.jurisdiction;
+          document.getElementById("acres").innerHTML = response.data.acres;
+          document.getElementById("maxElevation").innerHTML = response.data.maxElevation;
+          document.getElementById("percentSlope").innerHTML = response.data.percentSlope;
+          //document.getElementById('resultsDiv')
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    });
+
 });
