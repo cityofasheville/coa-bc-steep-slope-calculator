2f5acdb91aed82f0c54bee0deb56e5f43c80010a
Jon <jtwilson@ashevillenc.gov>
2021-03-30 10:16:21 -0400

    sqldocs

--- docs/database_funcs.sql
+++ docs/database_funcs.sql
@@ -1,176 +1,127 @@
 
-select * from sloperesults s 
-order by rundate desc
-limit 100
 
 
-  SELECT slopetool_checkGeomg( (SELECT st_union(p.shape) as GEOM
-  FROM bc_property p
-  WHERE  p.pinnum = any ('{962802744900000,964950354000000}') ) )as ID;
 
-----------------------------------  
-select st_union(p.shape) as GEOM from bc_property p
-WHERE  p.pinnum = any ('{962802744900000,964950354000000}')
-limit 100
 
-SELECT CASE WHEN (max(s."insertid")+1) is null THEN 1 ELSE (max(s."insertid")+1) END  from "sloperesults" s;
-----------------------------------
- -- 'slopetool_checkgeomg'
- CREATE OR REPLACE FUNCTION public.slopetool_checkgeomg(geom geometry)
- RETURNS bigint
+
+---------------------------------- NEW 2
+CREATE OR REPLACE FUNCTION public.slopetool_getsloperecfrompins(pins character varying[])
+ RETURNS TABLE(id integer, jurisdiction character varying, "maxElevation" integer, acres numeric, "percentSlope" numeric)
  LANGUAGE plpgsql
 AS $function$
-DECLARE theInsertID BIGINT;
-DECLARE ifExists BIGINT;
-
-BEGIN
-  SELECT CASE WHEN (max(s."insertid")+1) is null THEN 1 ELSE (max(s."insertid")+1) END into theInsertID from "sloperesults" s;
-  SELECT count(*) INTO ifExists FROM "sloperesults" WHERE ST_Equals(sloperesults.rungeom,geom) ;
-
-  IF ifExists < 1 THEN RETURN 0; End IF;
-
-  INSERT INTO sloperesults
-    (jurisdiction, maxcontour, acres, avgslope, length, rundate, insertid, rungeom)
-     (SELECT sloperesults.jurisdiction,
-            sloperesults.maxcontour,
-            sloperesults.acres,
-            sloperesults.avgslope,
-            sloperesults.length,
-            LOCALTIMESTAMP,
-            theInsertID,
-            sloperesults.rungeom
-     FROM sloperesults
-     WHERE ST_Equals(sloperesults.rungeom,geom) limit 1);
-
-
-  RETURN theInsertID;
-
-END;
-$function$
-;
-
-----------------------------------
--- slopetool_getslopefromgeom
-CREATE OR REPLACE FUNCTION public.slopetool_getslopefromgeom(geom geometry)
- RETURNS bigint
- LANGUAGE plpgsql
-AS $function$
-DECLARE theJuris TEXT;
-DECLARE theAvgSlope NUMERIC;
-DECLARE theArea NUMERIC;
-DECLARE theInsertID BIGINT;
-DECLARE theStartTime timestamp without time zone;
-DECLARE theEndTime timestamp without time zone;
-DECLARE theIntersectGEOM geometry;
-DECLARE theMaxC integer;
-BEGIN
-  SELECT LOCALTIMESTAMP into theStartTime;
-  SELECT slopetool_getjurisdictionG(geom) into theJuris;
-  SELECT slopetool_getareaG(geom) into theArea;
-  SELECT CASE WHEN (max(s."insertid")+1) is null THEN 1 ELSE (max(s."insertid")+1) END into theInsertID from "sloperesults" s;
-  INSERT INTO "sloperesults"
-  (jurisdiction, maxcontour, acres, avgslope, length, rundate, insertid, rungeom)
-  (SELECT
-         theJuris,
-         max(b."contour"),
-         theArea,
-         round((round(sum(ST_Length(st_intersection(b.shape, geom)))::numeric,2)/(theArea)*0.0115)::numeric,2),
-         sum(st_length(st_intersection(b.shape, geom))),
-         LOCALTIMESTAMP,
-         theInsertID,
-         st_multi(geom)
-  FROM "lidar_contours" b
-  WHERE st_intersects(b.shape,geom));
-  select LOCALTIMESTAMP into theEndTime;
-  RETURN theInsertID;
-END;
-$function$
-;
-----------------------------------
--- slopetool_getsloperecfromgeom
-CREATE OR REPLACE FUNCTION public.slopetool_getsloperecfromgeom(pins varchar[])
-	 RETURNS table (
-	 	id int8,
-	 	jurisdiction varchar(150),
-	 	"maxElevation" int4,
-	 	acres numeric(15,5),
-	 	"percentSlope" numeric(10,5)
-	 )
-	 LANGUAGE plpgsql
-	 AS $function$
-	 declare a_rec record;
-begin
-													return query 
-	SELECT st_union(p.shape) as GEOM
+    declare geom geometry;
+    DECLARE ifExists BIGINT;
+begin												
+	SELECT st_union(p.shape) into geom
 	FROM bc_property p
-	WHERE  p.pinnum in (pins);
-
-	--SELECT count(*) INTO a_rec FROM "sloperesults" WHERE ST_Equals(sloperesults.rungeom,geom) ;
-
-  --IF ifExists < 1 THEN RETURN 0; End IF;	 
-	 
-	 
-END;
+	WHERE  p.pinnum = any(pins);
+--
+	if geom is null then
+		return query(
+        SELECT 0::int, null::varchar, null::int4, null::numeric, null::numeric);
+	else
+	    SELECT count(*) INTO ifExists FROM sloperesults s where ST_Equals(s.rungeom,geom) ;
+	    IF ifExists < 1 then
+	        -- insert new record
+	        INSERT INTO "sloperesults"
+	        (jurisdiction, maxcontour, acres, avgslope, length, rundate, rungeom)
+	        (SELECT
+	        slopetool_getjurisdictionG(geom),
+	        max(b."contour"),
+	        slopetool_getareaG(geom),
+	        round((round(sum(ST_Length(st_intersection(b.shape, geom)))::numeric,2)/(slopetool_getareaG(geom))*0.0115)::numeric,2),
+	        sum(st_length(st_intersection(b.shape, geom))),
+	        LOCALTIMESTAMP,
+	        st_multi(geom)
+	        FROM "lidar_contours" b
+	        WHERE st_intersects(b.shape,geom));
+	    End IF;
+	-- 
+		return query(
+	        SELECT s.insertid as id ,s.jurisdiction,
+	        s.maxcontour as "maxElevation",
+	        round(s.acres,2) as acres,
+	        round(s.avgslope,2) as "percentSlope"
+	        FROM sloperesults s
+	        WHERE ST_Equals(s.rungeom,geom) 
+	    );
+	end if;
+END
 $function$
 ;
- 
- 
- ----
-  ----------------------------------
-drop FUNCTION public.testfun();
 
-CREATE OR REPLACE FUNCTION public.testfun()
-returns int
-language plpgsql
-as $$
-declare
-	pins varchar[] := array['962802744900000'];
+ ----------------------------------
+ select * from public.slopetool_getsloperecfrompins(array['964903212400000'])
+ 
+ ----------------------------------
+
+
+ DROP TABLE public.sloperesults;
+
+CREATE TABLE public.sloperesults (
+	jurisdiction varchar(150) NULL,
+	maxcontour int4 NULL,
+	acres numeric(15,5) NULL,
+	avgslope numeric(10,5) NULL,
+	length numeric(20,5) NULL,
+	rundate timestamp NULL,
+	insertid SERIAL,
+	runtime text NULL,
+	rungeom geometry(MULTIPOLYGON, 2264) NULL
+);
+
+-- Preload table
+drop function myFunc;
+CREATE FUNCTION myFunc() RETURNS void AS $$ 
+declare geom geometry;
+declare areag numeric;
+declare shapes cursor for 
+	SELECT shape --,pin
+	FROM bc_property
+	where left(pin,1) = '9'
+	;
 begin
-	--return query
-	return(select objectid from bc_property where pinnum = any(pins));
-end $$; 
-  ----------------------------------
-CREATE OR REPLACE FUNCTION public.testfun()
-	 RETURNS table (
-	 	res int
-	 )
-language plpgsql
-as $$
-declare
-	pins varchar[] := array['962802744900000'];
-begin
-	return query(select objectid from bc_property where pinnum = any(pins));
-end $$; 
-  ----------------------------------
-CREATE OR REPLACE FUNCTION public.testfun(pins varchar[])
-	 RETURNS table (
-	 	res int
-	 )
-language plpgsql
-as $$
-begin
-	return query(select objectid from bc_property where pinnum = any(pins));
-end $$;  
-   ----------------------------------
- select public.testfun(array['962802744900000']);
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+open shapes;
+loop
+	fetch shapes into geom;
+	exit when not found;
+	areag := slopetool_getareaG(geom);
+	if areag > 0 then
+	    INSERT INTO "sloperesults"
+	    (jurisdiction, maxcontour, acres, avgslope, length, rundate, rungeom)
+	    (SELECT
+	    slopetool_getjurisdictionG(geom),
+	    max(b."contour"),
+	    slopetool_getareaG(geom),
+	    round((round(sum(ST_Length(st_intersection(b.shape, geom)))::numeric,2)/(slopetool_getareaG(geom))*0.0115)::numeric,2),
+	    sum(st_length(st_intersection(b.shape, geom))),
+	    LOCALTIMESTAMP,
+	    st_multi(geom)
+	    FROM "lidar_contours" b
+	    WHERE st_intersects(b.shape,geom));
+	  end if;
+end loop;
+end;
+$$ LANGUAGE plpgsql;
+
+select myFunc()
+-----------------------------
+
+select slopetool_getareaG(shape), *
+from bc_property bp 
+where slopetool_getareaG(shape) = 0
+
+
+
+select count(*) FROM bc_property p;
+select count(*) FROM sloperesults s2 ;
+select count(*) FROM lidar_contours s2 ;
+
+
+
+     
+
+
+
+
+
--- docs/slopetool_getsloperecfrompins.sql
+++ docs/slopetool_getsloperecfrompins.sql
@@ -1,18 +1,14 @@
 -- slopetool_getsloperecfrompins
 drop FUNCTION public.slopetool_getsloperecfrompins;
-CREATE OR REPLACE FUNCTION public.slopetool_getsloperecfrompins(pins varchar[])
+
+
+CREATE OR REPLACE FUNCTION public.slopetool_getsloperecfrompins(pins character varying[])
 -- checks to see if slope already calculated
 -- and adds to table if not. Returns slope record.
 -- returns id of 0 if property not found.
-	 RETURNS table (
-	 	id int,
-	 	jurisdiction varchar(150),
-	 	"maxElevation" int4,
-	 	acres numeric(15,5),
-	 	"percentSlope" numeric(10,5)
-	 )
-    LANGUAGE plpgsql
-    AS $function$
+ RETURNS TABLE(id integer, jurisdiction character varying, "maxElevation" integer, acres numeric, "percentSlope" numeric)
+ LANGUAGE plpgsql
+AS $function$
     declare geom geometry;
     DECLARE ifExists BIGINT;
 begin												
@@ -20,14 +16,14 @@ begin
 	FROM bc_property p
 	WHERE  p.pinnum = any(pins);
 --
-	if geom is null then -- record not found in County property table
+	if geom is null then
 		return query(
         SELECT 0::int, null::varchar, null::int4, null::numeric, null::numeric);
 	else
-	    SELECT count(*) INTO ifExists FROM sloperesults2 s where ST_Equals(s.rungeom,geom) ;
+	    SELECT count(*) INTO ifExists FROM sloperesults s where ST_Equals(s.rungeom,geom) ;
 	    IF ifExists < 1 then
 	        -- insert new record
-	        INSERT INTO "sloperesults2"
+	        INSERT INTO "sloperesults"
 	        (jurisdiction, maxcontour, acres, avgslope, length, rundate, rungeom)
 	        (SELECT
 	        slopetool_getjurisdictionG(geom),
@@ -46,7 +42,7 @@ begin
 	        s.maxcontour as "maxElevation",
 	        round(s.acres,2) as acres,
 	        round(s.avgslope,2) as "percentSlope"
-	        FROM sloperesults2 s
+	        FROM sloperesults s
 	        WHERE ST_Equals(s.rungeom,geom) 
 	    );
 	end if;
