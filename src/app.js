2569c0c6c1fe40b1069eaeef1315caaebd1a9690
6foot5 <6foot5@gmail.com>
2024-12-30 13:35:40 -0500

    add CORS for dev URL

--- src/app.js
+++ src/app.js
@@ -14,22 +14,23 @@ app.set('views', new URL('./views/', import.meta.url).pathname); // join(__dirna
 app.set('view engine', 'hbs');
 
 var allowList = [
-  'https://simplicity.ashevillenc.gov', 
-  'http://simplicity.ashevillenc.gov', 
-  'https://dev-simplicity.ashevillenc.gov', 
-  'http://dev-simplicity.ashevillenc.gov', 
-  'https://localhost:3000', 
+  'https://simplicity.ashevillenc.gov',
+  'http://simplicity.ashevillenc.gov',
+  'https://dev-simplicity.ashevillenc.gov',
+  'http://dev-simplicity.ashevillenc.gov',
+  'https://steep-slope-dev.ashevillenc.gov',
+  'https://localhost:3000',
   'http://localhost:3000',
-  'https://climatej.d1thp43hcib1lz.amplifyapp.com'
+  'https://climatej.d1thp43hcib1lz.amplifyapp.com',
 ];
 var corsOptions = {
   origin: function (origin, callback) {
     if (allowList.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error('Not allowed by CORS'));
     }
-  }
+  },
 };
 
 app.use(cors(corsOptions));
@@ -40,7 +41,7 @@ app.use(cookieParser());
 const pubdir = new URL('./public/', import.meta.url).pathname;
 app.use(servestatic(pubdir));
 
-app.use(function(req,res,next) {
+app.use(function (req, res, next) {
   res.locals.client = app.get('client'); // db client
   next();
 });
@@ -50,7 +51,7 @@ app.use('/api', api);
 app.use('/find-slope-for-parcel', findslopeforparcel);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -61,22 +62,22 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: err
+      error: err,
     });
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    error: {},
   });
 });
 
